76b8fd5f4f9d7c9cdeff374d84d5f71e714c9d58
Kaisha Smith <smith.kaisha04@gmail.com>
2020-05-19 02:21:47 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,9 @@
 function basicTeenager(age) {
-  if (13 < age < 19) {
+  if (13 <= age <= 19) {
     return ("You are a teenager!");
 }
 }
-basicTeenager = 13;
+basicTeenager(13)
 
 function teenager(age) {
   if (13 <= age <= 19) {
